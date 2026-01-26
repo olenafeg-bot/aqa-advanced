f6c32dd9a3237891d0ccdae27dfe00f1ce47eb2d
Olena Fehetsyn <olena.fehetsyn@medtronic.com>
2026-01-26 14:43:36 +0200

    lesson 7 task 3 with fix

--- jsFunctionsAdvanced/task3.js
+++ jsFunctionsAdvanced/task3.js
@@ -1,18 +1,29 @@
 const divide = (numenator, denominator) => {
-   try {
-    if (denominator === 0){
-        return 'Error: Division by zero is not allowed.';
+      if (denominator === 0){
+        throw 'Error: Division by zero is not allowed.';
     } else if (isNaN(denominator) || isNaN(numenator)) {
-        return 'Error: Invalid input. Please provide numeric values.';
+        throw 'Error: Invalid input. Please provide numeric values.';
     } else {
         return numenator / denominator;
     }  
-    } catch (error) {
-        return `Error: ${error.message}`;
-    } finally {
-        console.log('Execution completed.');
-    }
 };
-console.log(divide(15, 5));
-console.log(divide(6, 0));
-console.log(divide(8, 'd'));
+try {
+    console.log(divide(15, 5));
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Execution completed.');
+} try {
+    console.log(divide(20,0));
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Execution completed.'); 
+} try {
+    console.log(divide(8,'d'));
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('Execution completed.');
+}
+
